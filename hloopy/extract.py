a671559239205d134a6f29ba9bb7aa236678c534
Julian Irwin <julian.irwin@gmail.com>
2016-12-15 22:00:52 -0600

    started adding tests

--- hloopy/extract.py
+++ hloopy/extract.py
@@ -1,8 +1,10 @@
 import numpy as np
 
+
 def coercivity(hloop):
     """Find the coercivity of an hloop, determined as the x-intercepts of
     each (centered with `y -= y.mean()`) branch.
+
     Args:
         hloop (hloopy.hloop): Hloop to be operated on
 
@@ -12,3 +14,23 @@ def coercivity(hloop):
              'indicies': (plus_side, minus_side)}
     """
     pass
+
+
+def remanence(hloop):
+    """Find the remanence of an hloop, determined as the y-intercepts of each
+    branch.
+
+    Args:
+        hloop (hloopy.hloop): Hloop to be operated on.
+        center (bool): If true, center the curve in the x-direction before
+                       computing the remanence.
+
+    Returns:
+        A dict with two elements:
+            {'remanence': (plus_side, minus_side),
+             'indicies': (plus_side, minus_side)}
+    """
+    
+    pass
+
+

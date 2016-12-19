559b155743e9966359f6f8382a7fb37634fa82f6
Julian Irwin <julian.irwin@gmail.com>
2016-12-19 12:14:38 -0600

    deleted unused find_all()

--- tests/test_util.py
+++ tests/test_util.py
@@ -5,6 +5,3 @@ from nose.tools import assert_equal
 def test_rightpad():
     assert_equal(rightpad('xy', '.', 3), 'xy.')
 
-
-def test_find_all():
-    assert_equal(list(find_all('x.yy', 'y')), [2, 3])
